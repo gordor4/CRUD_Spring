d76d17703c3754c435f8a61da48d87906d2d601f
mindubaev.rm <dcQim9R>
2020-04-07 19:59:25 +0300

    add base spring project

---
+++ src/test/java/ru/rus/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rus.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
